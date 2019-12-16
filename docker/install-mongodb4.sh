950e8b5b262107a1431417c461d33f53f280c559
zhouzhongqing <1030907690@qq.com>
2019-12-16 18:28:47 +0800

    docker

---
+++ docker/install-mongodb4.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+yum install -y docker
+systemctl start docker
+mkdir -p /data/mongodb/datadir
+docker run -p 27017:27017 --privileged=true -d --name mongo-standard  -v /data/mongodb/datadir:/data/db -e MONGO_INITDB_ROOT_USERNAME=root -e MONGO_INITDB_ROOT_PASSWORD=root mongo:4.0
---
+++ docker/install-redis5.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#redis5.0.7 密码root
+yum install -y docker wget
+systemctl start docker
+mkdir -p /data/redis/conf
+cd /data/redis/conf
+wget -c
+docker run -d -p 6379:6379 -v /data/redis/conf:/usr/local/etc/redis --name redis-standard redis:5.0.7 redis-server /usr/local/etc/redis/redis.conf
