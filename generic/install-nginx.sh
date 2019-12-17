2aea52df11141b78fb65ed595471b98f9704d229
zhouzhongqing <1030907690@qq.com>
2019-12-17 16:41:42 +0800

    nginx

--- generic/install-nginx.sh
+++ generic/install-nginx.sh
@@ -19,3 +19,6 @@ tar -zxvf zlib-1.2.11.tar.gz
 cd nginx-1.12.2
 ./configure --prefix=/usr/local/nginx  --with-pcre=../pcre-8.41 --with-zlib=../zlib-1.2.11 --with-http_ssl_module   --with-openssl=../openssl-1.0.2q
 make && make install
+cd /usr/local/nginx/conf
+rm -rf nginx.conf
+wget -c https://raw.githubusercontent.com/1030907690/public-script/master/generic/nginx.conf
