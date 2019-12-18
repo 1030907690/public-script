38298ff7f45f435840abde8b38a515f5b335e112
zhouzhongqing <1030907690@qq.com>
2019-12-18 10:51:17 +0800

    追加字符串

---
+++ generic/install-openresty.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+yum install -y wget  gcc  gcc-c++ g++
+#基础依赖
+wget -c https://liquidtelecom.dl.sourceforge.net/project/generic-software/php/openresty-1.15.8.2.tar.gz
+wget -c https://liquidtelecom.dl.sourceforge.net/project/generic-software/php/openssl-1.0.2q.tar.gz
+wget -c https://liquidtelecom.dl.sourceforge.net/project/generic-software/php/pcre-8.41.tar.gz
+wget -c https://liquidtelecom.dl.sourceforge.net/project/generic-software/php/zlib-1.2.11.tar.gz
+wget -c https://liquidtelecom.dl.sourceforge.net/project/generic-software/php/perl-5.30.1.tar.gz
+
+#nginx 打印
+wget -c https://liquidtelecom.dl.sourceforge.net/project/generic-software/php/echo-nginx-module-0.61.tar.gz
+
+
+
+tar -xzf perl-5.30.1.tar.gz
+cd perl-5.30.1
+./Configure -des -Dprefix=$HOME/localperl
+make
+make test
+make install
+cd ..
+tar -zxvf echo-nginx-module-0.61.tar.gz
+tar -zxvf openresty-1.15.8.2.tar.gz
+tar -zxvf openssl-1.0.2q.tar.gz
+tar -zxvf pcre-8.41.tar.gz
+tar -zxvf zlib-1.2.11.tar.gz
+
+cd openresty-1.15.8.2
+#--add-module=../echo-nginx-module-0.61
+./configure --prefix=/usr/local/openresty --with-pcre=../pcre-8.41 --with-zlib=../zlib-1.2.11 --with-http_ssl_module   --with-openssl=../openssl-1.0.2q
+make && make install
+cd /usr/local/openresty
+mkdir nginx/conf/vhost
+#追加字符串
+sed -i 'N;116 a include vhost/*.conf;' nginx/conf/nginx.conf
