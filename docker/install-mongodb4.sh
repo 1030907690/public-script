0c53dfd75758de3ad0ff9157b45d53d048e39e84
zhouzhongqing <1030907690@qq.com>
2019-12-18 11:00:24 +0800

    密码

--- docker/install-mongodb4.sh
+++ docker/install-mongodb4.sh
@@ -3,3 +3,4 @@ yum install -y docker
 systemctl start docker
 mkdir -p /data/mongodb/datadir
 docker run -p 27017:27017 --privileged=true -d --name mongo-standard  -v /data/mongodb/datadir:/data/db -e MONGO_INITDB_ROOT_USERNAME=root -e MONGO_INITDB_ROOT_PASSWORD=root mongo:4.0
+echo "mongodb密码root"
