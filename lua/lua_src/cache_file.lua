88845264e7644c88109f1583ee9f8efb2605b277
zhouzhongqing <1030907690@qq.com>
2019-12-23 14:30:24 +0800

    缓存

--- lua/lua_src/cache_file.lua
+++ lua/lua_src/cache_file.lua
@@ -1,35 +1,45 @@
 -- 缓存文件
  local _M = {}
-cache_tab = {};
+cache_tab = {}
 function _M.cache()
-     local existence = false
+
+    local existence = false
     -- 静态文件前缀
     local prefix = "/usr/local/openresty/nginx/html"
+	-- 刷新缓存uri
+	local refresh = "/test/refresh.lua"
     local httpUri = ngx.var.request_uri
-    if nil == cache_tab[httpUri] then
-	
-	    local file = io.open(prefix..httpUri, "rb")
-		if file then
-			local ret = file:read("*a")
-            if nil ~= ret then
-				cache_tab[httpUri] = ret
-				existence = true
-				-- ngx.say("save cache")
-			end
-			file:close()
-		else
-		    existence = false
-           -- ngx.say("404 Not Found")
-		end
-    else
-        existence = true
-    end
 
-    -- 打印结果
-    if existence then
-        ngx.say(cache_tab[httpUri])
-    else
-        ngx.say("404 Not Found")
-    end
+	if refresh == httpUri then
+        -- 缓存清空
+		cache_tab = {}
+		ngx.say("cache refresh successful")
+	else
+		if nil == cache_tab[httpUri] then
+			local file = io.open(prefix..httpUri, "rb")
+			if file then
+				local ret = file:read("*a")
+				if nil ~= ret then
+                    -- 保存缓存
+					cache_tab[httpUri] = ret
+					existence = true
+					-- ngx.say("save cache")
+				end
+				file:close()
+			else
+                -- 没找到文件
+				existence = false
+			   -- ngx.say("404 Not Found")
+			end
+		else
+			existence = true
+		end
+		-- 打印结果
+		if existence then
+			ngx.say(cache_tab[httpUri])
+		else
+			ngx.say("404 Not Found")
+		end
+	end
 end
 return _M
