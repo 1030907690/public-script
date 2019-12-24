ee048c3396073acc869ef66bfcee49b2d47b88f2
zhouzhongqing <1030907690@qq.com>
2019-12-24 10:50:40 +0800

    pypi

--- generic/global-pypi.sh
+++ generic/global-pypi.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+mkdir ~/.pip
 touch ~/.pip/pip.conf
 echo "[global] \n index-url = https://mirrors.aliyun.com/pypi/simple/\n [install] \ntrusted-host=mirrors.aliyun.com" > ~/.pip/pip.conf
 touch ~/.pip/pip.conf
