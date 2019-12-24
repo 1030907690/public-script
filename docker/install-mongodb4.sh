1d66bc963f69696d29f366f622fa083e194d754b
zhouzhongqing <1030907690@qq.com>
2019-12-24 16:46:46 +0800

    db

--- docker/install-mongodb4.sh
+++ docker/install-mongodb4.sh
@@ -4,4 +4,4 @@ systemctl start docker
 mkdir -p /data/mongodb/datadir
 docker run -p 27017:27017 --privileged=true -d --name mongo-standard  -v /data/mongodb/datadir:/data/db -e MONGO_INITDB_ROOT_USERNAME=root -e MONGO_INITDB_ROOT_PASSWORD=root mongo:4.0
 echo "mongodb用户名密码都是密码root"
-echo "登录进入容器可以用命令 mongo --port 27017 -u "root" -p "root" --authenticationDatabase \"admin\" 连接"
+echo "登录进入容器可以用命令 mongo --port 27017 -u \"root\" -p \"root\" --authenticationDatabase \"admin\" 连接"
