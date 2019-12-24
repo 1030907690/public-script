bd61d33f87791907b704d71786687698c8e89e63
zhouzhongqing <1030907690@qq.com>
2019-12-24 16:25:15 +0800

    zookeeper

---
+++ docker/install-zookeeper3.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#安装zookeeper3.4
+yum install -y docker wget
+systemctl start docker
+mkdir -p /data/zookeeper/conf
+cd /data/zookeeper/conf
+wget -c https://raw.githubusercontent.com/1030907690/public-script/master/docker/zoo.cfg
+docker pull zookeeper:3.4
+docker run --name zookeeper-standard --restart always -d -v /data/zookeeper/conf/zoo.cfg:/conf/zoo.cfg zookeeper:3.4
