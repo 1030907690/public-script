23544f5093320c7a74e779ce2924e9f02dac9e9e
zhouzhongqing <1030907690@qq.com>
2019-12-24 16:39:11 +0800

    redis

--- docker/install-redis5.sh
+++ docker/install-redis5.sh
@@ -5,5 +5,5 @@ systemctl start docker
 mkdir -p /data/redis/conf
 cd /data/redis/conf
 wget -c https://raw.githubusercontent.com/1030907690/public-script/master/docker/redis.conf
-docker run  --privileged=true -d -p 6379:6379 -v /data/redis/conf:/usr/local/etc/redis --name redis-standard redis:5.0.7 redis-server /usr/local/etc/redis/redis.conf
+docker run  --privileged=true -d -p 6379:6379 -v /data/redis/conf/redis.conf:/usr/local/etc/redis/redis.conf --name redis-standard redis:5.0.7 redis-server /usr/local/etc/redis/redis.conf
 echo "redis密码root"
