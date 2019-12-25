d6c5557b3e9c4656c4bb2a7ebc22fb0eb240e592
zhouzhongqing <1030907690@qq.com>
2019-12-25 22:10:04 +0800

    短链接

---
+++ lua/lua_src/short/short_url.lua
@@ -0,0 +1,40 @@
+
+local _M = {}
+function _M.short_url()
+ local args = ngx.req.get_uri_args()
+ local http_uri = ngx.var.request_uri
+ local short_url_uri = "/short.do"
+
+ if  nil ~= string.find(http_uri,short_url_uri) then
+	
+ 
+		ngx.header.content_type = 'text/json'
+		local functions = require('short/functions')
+		-- local cjson = require('cjson')
+		local args = ngx.req.get_uri_args()
+
+		local long_url = args['url']
+		local short_string = args['short']
+		--ngx.say("url   "..long_url)
+		local short_url_val, err = functions.url_create(long_url)
+		if err then
+			functions.show_error(err)
+		end
+		ngx.say('{"status":1,"shorturl":"'..short_url_val..'"}')
+ else
+	--ngx.say("重定向")
+	ngx.header.content_type = 'text/html'
+	local config = require('short/config')
+	local functions = require('short/functions')
+	local short_string = string.sub(ngx.var.uri, -#config['start_url'])
+	 
+	local long_url, err = functions.get_long_url(short_string)
+	if err then
+		functions.show_error(err)
+	end
+	ngx.redirect(long_url)
+ end
+ 
+ 
+end
+return _M
--- lua/lua_src/short_url.lua
+++
@@ -1,7 +0,0 @@
- local _M = {}
-cache_str = nil;
-function _M.short_url()
- local args = ngx.req.get_uri_args()
-
-end
-return _M
