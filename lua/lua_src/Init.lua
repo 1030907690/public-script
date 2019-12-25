cd94dd2d25c08975d8c1f602a31148b90df0373f
zhouzhongqing <1030907690@qq.com>
2019-12-25 20:47:08 +0800

    短链接

--- lua/lua_src/Init.lua
+++ lua/lua_src/Init.lua
@@ -1,5 +1,6 @@
-local p = "/usr/local/openresty/nginx/conf/lua_src"
+local p = "/usr/local/openresty/nginx/conf/lua_src/"
 local m_package_path = package.path
 package.path = string.format("%s?.lua;%s?/init.lua;%s", p, p, m_package_path)
 cmd = require("t")
 cache = require("cache_file")
+short_cmd = require("short_url")
---
+++ lua/lua_src/short_url.lua
@@ -0,0 +1,7 @@
+ local _M = {}
+cache_str = nil;
+function _M.short_url()
+ local args = ngx.req.get_uri_args()
+
+end
+return _M
