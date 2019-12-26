d64da50a691f74b25c863e0f359c0833fd68b860
zhouzhongqing <1030907690@qq.com>
2019-12-26 18:30:30 +0800

    短链接生成

--- lua/lua_src/short/short_url.lua
+++ lua/lua_src/short/short_url.lua
@@ -4,13 +4,14 @@ function _M.short_url()
  local args = ngx.req.get_uri_args()
  local http_uri = ngx.var.request_uri
  local short_url_uri = "/short.do"
+ local root_uri = "/"
 
  if  nil ~= string.find(http_uri,short_url_uri) then
-	
+
 		ngx.header.content_type = 'text/json'
 		local functions = require('short/functions')
 		-- local cjson = require('cjson')
-		local args = ngx.req.get_uri_args()
+		-- local args = ngx.req.get_uri_args()
 
 		local long_url = args['url']
 		local short_string = args['short']
@@ -19,8 +20,12 @@ function _M.short_url()
 		if err then
 			functions.show_error(err)
 		end
-		ngx.say('{"status":1,"shorturl":"'..short_url_val..'"}')	
- 
+		ngx.say('{"status":1,"shorturl":"'..short_url_val..'"}')
+
+ elseif root_uri == http_uri then
+	ngx.header.content_type = 'text/html'
+	local error_html = "<html> <body> <center> <img src=\"https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&amp;quality=100&amp;size=b4000_4000&amp;sec=1577355764&amp;di=0fa924810ceba83a0835e8b4c7f15a58&amp;src=http://n.sinaimg.cn/sinacn17/267/w560h507/20180716/855e-hfkffak7000432.jpg\"></center> </body> </html>"
+	ngx.say(error_html)
  else
 	--ngx.say("重定向")
 	ngx.header.content_type = 'text/html'
