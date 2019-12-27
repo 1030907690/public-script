5ed7c11e0d4478719891abe9092cb775f8e26a3c
zhouzhongqing <1030907690@qq.com>
2019-12-27 10:48:23 +0800

    start

--- docker/install-gitlab.sh
+++ docker/install-gitlab.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #yum install -y docker
+systemctl start docker
 mkdir -p /data/gitlab
 docker run --detach  --hostname gitlab.example.com --publish 443:443 --publish 8881:80 --publish 23:22  --name gitlab-standard   --restart always  --volume /data/gitlab/config:/etc/gitlab   --volume /data/gitlab/logs:/var/log/gitlab   --volume /data/gitlab/data:/var/opt/gitlab  docker.io/gitlab/gitlab-ce
 echo "端口对应关系  443:443 8881:80  23:22 "
