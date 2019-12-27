468d1c04e8f2a1eb3613ab4903b80c39cd868cd3
zhouzhongqing <1030907690@qq.com>
2019-12-27 09:51:54 +0800

    docker

---
+++ docker/install-docker.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#yum install -y docker
+yum install -y yum-utils   device-mapper-persistent-data   lvm2
+yum-config-manager  --add-repo   https://download.docker.com/linux/centos/docker-ce.repo
+yum-config-manager --enable docker-ce-edge
+yum-config-manager --enable docker-ce-test
+yum install -y docker-ce
+systemctl start docker
