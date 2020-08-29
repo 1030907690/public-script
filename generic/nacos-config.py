094c9dc921401c7c7622e36d05bcfbf4b1b2d14e
1030907690 <1030907690@qq.com>
2020-08-29 09:43:41 +0800

    seate配置导入到nacos

---
+++ generic/nacos-config.py
@@ -0,0 +1,39 @@
+# -*-coding: UTF-8 -*-
+
+'''
+@author zhouzhongqing
+seate配置导入到nacos
+2020年8月29日09:36:25
+'''
+import sys
+import http.client
+
+if __name__ == '__main__':
+    headers = {
+        'content-type': "application/x-www-form-urlencoded"
+    }
+    hasError = False
+    for line in open('config.txt'):
+        pair = line.split('=')
+        if len(pair) < 2:
+            continue
+        print(line),
+        url_prefix = sys.argv[1]
+        conn = http.client.HTTPConnection(url_prefix)
+        if len(sys.argv) == 3:
+            namespace = sys.argv[2]
+            url_postfix = '/nacos/v1/cs/configs?dataId={0}&group=SEATA_GROUP&content={1}&tenant={2}'.format(
+                str(pair[0]), str(line[line.index('=') + 1:]).strip(), namespace)
+        else:
+            url_postfix = '/nacos/v1/cs/configs?dataId={}&group=SEATA_GROUP&content={}'.format(str(pair[0]), str(
+                line[line.index('=') + 1:])).strip()
+        conn.request("POST", url_postfix, headers=headers)
+        res = conn.getresponse()
+        data = res.read()
+        if data.decode("utf-8") != "true":
+            hasError = True
+    if hasError:
+        print("init nacos config fail.")
+    else:
+        print("init nacos config finished, please start seata-server.")
+    input('Enter any key to end')
