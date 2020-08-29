d83b6cfbbb3562e1be9326e677a131b58827260c
1030907690 <1030907690@qq.com>
2020-08-29 10:05:25 +0800

    seate配置导入到nacos

--- generic/nacos-config.py
+++ generic/nacos-config.py
@@ -7,33 +7,39 @@ seate配置导入到nacos
 '''
 import sys
 import http.client
+import os
+
 
 if __name__ == '__main__':
     headers = {
         'content-type': "application/x-www-form-urlencoded"
     }
     hasError = False
-    for line in open('config.txt'):
-        pair = line.split('=')
-        if len(pair) < 2:
-            continue
-        print(line),
-        url_prefix = sys.argv[1]
-        conn = http.client.HTTPConnection(url_prefix)
-        if len(sys.argv) == 3:
-            namespace = sys.argv[2]
-            url_postfix = '/nacos/v1/cs/configs?dataId={0}&group=SEATA_GROUP&content={1}&tenant={2}'.format(
-                str(pair[0]), str(line[line.index('=') + 1:]).strip(), namespace)
+    file_name = 'config.txt'
+    if os.path.exists(file_name):
+        for line in open(file_name):
+            pair = line.split('=')
+            if len(pair) < 2:
+                continue
+            print(line),
+            url_prefix = sys.argv[1]
+            conn = http.client.HTTPConnection(url_prefix)
+            if len(sys.argv) == 3:
+                namespace = sys.argv[2]
+                url_postfix = '/nacos/v1/cs/configs?dataId={0}&group=SEATA_GROUP&content={1}&tenant={2}'.format(
+                    str(pair[0]), str(line[line.index('=') + 1:]).strip(), namespace)
+            else:
+                url_postfix = '/nacos/v1/cs/configs?dataId={}&group=SEATA_GROUP&content={}'.format(str(pair[0]), str(
+                    line[line.index('=') + 1:])).strip()
+            conn.request("POST", url_postfix, headers=headers)
+            res = conn.getresponse()
+            data = res.read()
+            if data.decode("utf-8") != "true":
+                hasError = True
+        if hasError:
+            print("init nacos config fail.")
         else:
-            url_postfix = '/nacos/v1/cs/configs?dataId={}&group=SEATA_GROUP&content={}'.format(str(pair[0]), str(
-                line[line.index('=') + 1:])).strip()
-        conn.request("POST", url_postfix, headers=headers)
-        res = conn.getresponse()
-        data = res.read()
-        if data.decode("utf-8") != "true":
-            hasError = True
-    if hasError:
-        print("init nacos config fail.")
+            print("init nacos config finished, please start seata-server.")
     else:
-        print("init nacos config finished, please start seata-server.")
+        print(os.getcwd() + "/" + file_name + " not found. ")
     input('Enter any key to end')
