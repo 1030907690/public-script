1111a755f4f553a493cfa77ed1b4417f21e0ee4a
1030907690@qq.com <1030907690@qq.com>
2021-02-23 09:28:44 +0800

    注释

--- docker/install-gitlab.sh
+++ docker/install-gitlab.sh
@@ -2,5 +2,6 @@
 #yum install -y docker
 systemctl start docker
 mkdir -p /data/gitlab
+# 如果有需要可以自己改下 hostname gitlab.example.com,改成自己的,不改也没太多影响
 docker run --detach  --hostname gitlab.example.com --publish 444:443 --publish 8881:80 --publish 23:22  --name gitlab-standard   --restart always  --volume /data/gitlab/config:/etc/gitlab   --volume /data/gitlab/logs:/var/log/gitlab   --volume /data/gitlab/data:/var/opt/gitlab  docker.io/gitlab/gitlab-ce
-echo "端口对应关系  443:443 8881:80  23:22 "
+echo "端口对应关系  444:443 8881:80  23:22 "
