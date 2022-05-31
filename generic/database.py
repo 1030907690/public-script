9795e18c1649b7b94d49fcb4cfcb76d02f767125
pn20120162 <pn20120162>
2022-05-31 13:58:38 +0800

    feat:读取本地配置

--- generic/database.py
+++ generic/database.py
@@ -13,22 +13,31 @@ https://blog.csdn.net/qq_35427589/article/details/121767760
 '''
 
 import pymysql
+import os
+import json
 
-host = '172.19.0.192'
+host = ''
 
 port = 2881
 user = 'root'
-passwd = 'root@gs123'
+passwd = ''
 db = 'scrm'
 charset = 'UTF8'
+
+# 当前用户HOME目录
+file_prefix = os.path.expanduser('~')
+# 配置文件全路径
+config_path = file_prefix + '/database.json'
+
+
 
 
 def get_all_field(table_name):
     conn = pymysql.connect(host=host, port=port, user=user, passwd=passwd, db=db,
                            charset=charset);
-    cur = conn.cursor();
+    cur = conn.cursor()
     sql = "select group_concat(COLUMN_NAME) '所有字段' from information_schema.COLUMNS where table_name = '" + table_name + "'"
-    cur.execute(sql);
+    cur.execute(sql)
     # 获取所有记录列表
     results = cur.fetchall()
     all_field_origin = results[0][0]
@@ -68,8 +77,29 @@ def transfer_bean_field(all_field_origin_array):
             item_cache.append(item)
     return item_cache
 
+
+def read_and_set_config():
+
+    f = open(config_path, encoding="utf-8")
+    # 输出读取到的数据
+    text = f.read()
+    print(text)
+    config_obj = json.loads(text)
+    global host
+    global passwd
+    host = config_obj['host']
+    passwd = config_obj['passwd']
+    # 关闭文件
+    f.close()
+
+
 if __name__ == '__main__':
     print('start')
-    table_name = input('请输入表名?')
-    get_all_field(table_name)
+
+    read_and_set_config()
+
+    while True:
+        table_name = input('请输入表名?')
+        get_all_field(table_name)
+
     print('end')
