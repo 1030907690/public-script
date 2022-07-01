0bdba2aa100cb8b37752434e6543a3726aa74829
zzq <zzq>
2022-07-01 09:42:22 +0800

    feat:接口测试

--- generic/request.py
+++ generic/request.py
@@ -3,12 +3,14 @@ import requests
 import json
 
 if __name__ == '__main__':
-    # res = requests.get('http://127.0.0.1:25081/api/mars/base/sop/communicateRecord/code?parentCode=1')
+    # res = requests.get('')
     # print(res.text)
 
     data = {
-        'employeeIds':['xx']
+
     }
-    res = requests.post('http://127.0.0.1:25081/api/mars/base/groupChat/getCustomerList',data=json.dumps(data),
+    res = requests.post('',data=json.dumps(data),
                         headers={"Content-Type": "application/json; charset=UTF-8"})
     print(res.text)
+
+    # requests.put()
