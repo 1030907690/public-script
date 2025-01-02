680ae5f0df8f91c2c5c8d6213913fa3fb05dcb77
zhouzhongqing <zhouzhongqing@qq.com>
2025-01-02 11:09:36 +0800

    feat analysis_occupy.py

---
+++ generic/analysis_occupy.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+'''
+分析占用
+'''
+
+import os
+
+from decimal import Decimal, getcontext
+
+def get_folder_size(folder_path):
+    total_size = 0
+    errors = []
+    for folder_name, subfolders, filenames in os.walk(folder_path):
+        for filename in filenames:
+            file_path = os.path.join(folder_name, filename)
+            try:
+                total_size += os.path.getsize(file_path)
+            except FileNotFoundError as e:
+                errors.append(e)
+    return total_size
+
+
+
+
+if __name__ == '__main__':
+    getcontext().prec = 2
+    folder_path = 'C:/Users/Administrator/AppData'
+    gb_files = []
+    for item_path in os.listdir(folder_path):
+        full_path = folder_path +"/"+ item_path
+        size = get_folder_size(full_path)
+        mb = Decimal(size) / Decimal(1024) / Decimal(1024)
+        if mb > 100:
+            gb = Decimal(mb) / Decimal(1024)
+            print(f"{full_path} 文件或文件夹大小为 {gb}GB")
+            gb_files.append(f"{full_path} 文件或文件夹大小为 {gb}GB")
+        else:
+            print(f"{full_path} 文件或文件夹大小为 {mb}MB")
+
+
+    print("GB文件或文件夹")
+    for gb in gb_files:
+        print(gb)
