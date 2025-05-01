598e963a17505eaee0236d15c1192111abd6852e
zhouzhongqing <zhouzhongqing@qq.com>
2025-05-01 14:23:00 +0800

    feat:ddl

---
+++ generic/ddl_sql.py
@@ -0,0 +1,21 @@
+import pymysql
+
+if __name__ == '__main__':
+    conn = pymysql.connect(host='localhost', port=3306, user='root', passwd='root', db='jjjy181', charset='utf8mb4')
+    cur = conn.cursor()
+
+    # 新增
+    #	cur.execute("insert into tab1(tab1_id,val) VALUES (3,3)");
+    # 查询mysql版本	cur.execute("SELECT VERSION()");
+
+    # SQL 查询语句
+    sql = "show create table eb_user";
+    cur.execute(sql);
+    # 获取所有记录列表
+    results = cur.fetchall();
+    for row in results:
+        # print(row,"：result");
+        # print('result:', row);
+        print('result:', row[1]);
+    cur.close()
+    conn.close()
